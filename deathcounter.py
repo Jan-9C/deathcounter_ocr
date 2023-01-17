7cff3e60c776b80d26b4fc55fec8fe53aa288ba7
Jan-9C <jan.f388@gmail.com>
2023-01-17 19:19:28 +0100

    improved masks + added colorpicker
    Source colorpicker: https://answers.opencv.org/question/134248/how-to-define-the-lower-and-upper-range-of-a-color/

--- deathcounter.py
+++ deathcounter.py
@@ -82,8 +82,8 @@ def update_counter():
     detected = False
     # take screenshot using pyautogui
     image = pyautogui.screenshot()
-    # Turn image grayscale
-    image = cv2.cvtColor(np.array(image),cv2.COLOR_BGR2GRAY)
+    
+    image = cv2.cvtColor(np.array(image),cv2.COLOR_BGR2HSV_FULL)
      # Image crop coodinates
     x=754
     y=500
@@ -91,15 +91,58 @@ def update_counter():
     height=90
     # Crop the image
     image = image[y:y+height, x:x+width]
+   
+    cv2.imwrite("cropped.png", image)
+    
+    # Lower Mask for testimage1 : 
+    lower_red = np.array([176,183,102])
+    upper_red = np.array([166,173,62])
+    mask0 = cv2.inRange(image, lower_red, upper_red)
+    
+    # upper mask for testimage1:
+    lower_red = np.array([166,173,62])
+    upper_red = np.array([186,193,142])
+    mask1 = cv2.inRange(image, lower_red, upper_red)
+    
+    # Lower Mask for testimage2: 
+    lower_red = np.array([175,232,112])
+    upper_red = np.array([165,222,72])
+    mask2 = cv2.inRange(image, lower_red, upper_red)
+    
+    # upper Mask for testimage2 : 
+    lower_red = np.array([165,222,72])
+    upper_red = np.array([185,242,152])
+    mask3 = cv2.inRange(image, lower_red, upper_red)
+    
+    
+    
+    
+    
+    mask = mask0+mask1+mask2+mask3
+    
+    output_img = image.copy()
+    output_img[np.where(mask==0)] = 0
+    
+    image = output_img
+    
+    
+    
+    
+    cv2.imwrite("mask.png", image)
+    
+    # Turn image grayscale
+    image = cv2.cvtColor(np.array(image),cv2.COLOR_BGR2GRAY)
+    cv2.imwrite("image_grayscale.png", image)
+    
     # Black and White processing
     image = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    # Apply dilation and erosion to remove noise
+    #Apply dilation and erosion to remove noise
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
     image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=2)
 
     # Read text from image
-    imgtext = pytesseract.image_to_string(image, lang='eng', config='--psm 11')
-    ldistance = levenshtein(imgtext,"YOU DIED")
+    imgtext = pytesseract.image_to_string(image, lang='eng', config='--psm 11 --oem 3 -c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ -c tessedit_pageseg_mode=1 -c tessedit_min_word_length=2')
+    ldistance = levenshtein(imgtext,"YOUDIED")
     print("Detected: " + imgtext)
     print("ldistance: " + str(ldistance))
     
@@ -126,6 +169,7 @@ def update_counter():
         cv2.imwrite("image1.png", image)
         detected = True
         
+    cv2.imwrite("image1.png", image)
 
     if running & detected:
         root.after(10000, update_counter)
