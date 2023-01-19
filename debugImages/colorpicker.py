910632971235ef40043e370203921819d0857921
Jan-9C <jan.f388@gmail.com>
2023-01-19 11:32:50 +0100

    export masks now only by right clicking the opened mask window

--- debugImages/colorpicker.py
+++ debugImages/colorpicker.py
@@ -1,30 +1,28 @@
 import cv2
 import numpy as np
 import json
+import sys
 
 # THIS SCRIPT WAS NOT WRITTEN BY ME SOURCE: https://answers.opencv.org/question/134248/how-to-define-the-lower-and-upper-range-of-a-color/
 image_hsv = None   # global ;(
 pixel = (20,60,80) # some stupid default
-maskOpened = False
 data = {'lower': [-1,-1,-1], 'pixel': [-1,-1,-1], 'upper': [-1,-1,-1]}
 
 def exportData():
     global data
     print("Exporting data to gmask.json")
     json_obj = json.dumps(data)
-    with open("generatedMasks\gmask.json", "w") as f:
+    with open("generatedMasks\\" + sys.argv[2] + ".json", "w") as f:
         f.write(json_obj) 
+    cv2.destroyAllWindows()
 
 def closeMask(event, x,y, flags, param):
-    global maskOpened
     if event== cv2.EVENT_RBUTTONDOWN:
         cv2.destroyWindow("mask")
         exportData()
-        maskOpened = False
 
 # mouse callback function
 def pick_color(event,x,y,flags,param):
-    global maskOpened
     global data
     if event == cv2.EVENT_LBUTTONDOWN:
         pixel = image_hsv[y,x]
@@ -42,18 +40,17 @@ def pick_color(event,x,y,flags,param):
       
         image_mask = cv2.inRange(image_hsv,lower,upper)
         cv2.imshow("mask",image_mask)
-        maskOpened = True
         cv2.setMouseCallback("mask", closeMask )
-    elif event == cv2.EVENT_RBUTTONDOWN and maskOpened:
-        cv2.destroyWindow("mask")
-        maskOpened = False
-        exportData()
+    
 
 def main():
-    import sys
-    global image_hsv, pixel, maskOpened # so we can use it in mouse callback
-
-    maskOpened = False
+    
+    global image_hsv, pixel  # so we can use it in mouse callback
+    
+    if len(sys.argv) < 3:
+        print("Usage: python colorpicker.py <input_file> <export_file>")
+        print("export file without .json")
+        exit(0)
     
     image_src = cv2.imread(sys.argv[1])  # pick.py my.png
     if image_src is None:
